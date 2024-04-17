bcb77c06db71f34a27795dacbfc1d99a8b32f780
Dhananjay Yelwande <yelwandedhananjay@gmail.com>
2024-04-18 02:45:44 +0530

    fix

--- frontend/Wathare/api/mongo-proxy.js
+++ frontend/Wathare/api/mongo-proxy.js
@@ -1,16 +1,28 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
-  try {
-    const response = await axios({
-      method: req.method,
-      url: 'https://ap-south-1.aws.data.mongodb-api.com/app/data-iogss/endpoint/data/v1/action/findOne',
-      headers: {
-        'Content-Type': 'application/json',
-        'api-key': process.env.MONGODB_API
-      },
-      data: req.body
+    var data = JSON.stringify({
+        "collection": "mySensorData",
+        "database": "Wathare",
+        "dataSource": "Wathare",
+        "projection": {
+            "_id": 1
+        }
     });
+
+    var config = {
+        method: 'post',
+        url: 'https://ap-south-1.aws.data.mongodb-api.com/app/data-iogss/endpoint/data/v1/action/findOne',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Request-Headers': '*',
+          'api-key': '0qaAC8RoGqWQ3yPaxgLhP9d0fHTVVR9jkBcaZ5ug30RKlfw8p0o9QEBWh7Pt9C9z',
+        },
+        data: data
+    };
+    
+  try {
+    const response = await axios(config)
     res.status(response.status).json(response.data);
   } catch (error) {
     console.error(error);
