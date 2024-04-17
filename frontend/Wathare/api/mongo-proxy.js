0540dce871eeeefd8d6ac9e9e2bd1a741ef186d1
Dhananjay Yelwande <yelwandedhananjay@gmail.com>
2024-04-18 05:18:02 +0530

    fix

--- frontend/Wathare/api/mongo-proxy.js
+++ frontend/Wathare/api/mongo-proxy.js
@@ -1,47 +1,47 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default async function handler(req, res) {
-    const { startTime, frequency } = req.body;
-    console.log("startTime: " + startTime);
-    console.log("frequency: " + frequency);
-    
+  const { startTime, frequency } = req.body;
+  console.log("startTime: " + startTime);
+  console.log("frequency: " + frequency);
 
-    var data = JSON.stringify({
-        "collection": "mySensorData",
-        "database": "Wathare",
-        "dataSource": "Wathare",
-        "projection": {
-            "_id": 1,
-            "ts": 1,
-            "machine_status": 1,
-            "vibration": 1
-        },
-        "limit": 50000
-    });
+  var data = JSON.stringify({
+    collection: "mySensorData",
+    database: "Wathare",
+    dataSource: "Wathare",
+    projection: {
+      _id: 1,
+      ts: 1,
+      machine_status: 1,
+      vibration: 1,
+    },
+    limit: 50000,
+  });
 
-    var config = {
-        method: 'post',
-        url: 'https://ap-south-1.aws.data.mongodb-api.com/app/data-iogss/endpoint/data/v1/action/find',
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Request-Headers': '*',
-          'api-key': '0qaAC8RoGqWQ3yPaxgLhP9d0fHTVVR9jkBcaZ5ug30RKlfw8p0o9QEBWh7Pt9C9z',
-        },
-        data: data
-    };
-    
+  var config = {
+    method: "post",
+    url: "https://ap-south-1.aws.data.mongodb-api.com/app/data-iogss/endpoint/data/v1/action/find",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Request-Headers": "*",
+      "api-key":
+        "0qaAC8RoGqWQ3yPaxgLhP9d0fHTVVR9jkBcaZ5ug30RKlfw8p0o9QEBWh7Pt9C9z",
+    },
+    data: data,
+  };
+
   try {
-    const response = await axios(config)
+    const response = await axios(config);
+    console.log("response: " + JSON.stringify(response.data));
+    console.log("type of response: " + typeof(response.data));
 
     if (!frequency && !startTime) {
-        res.status(response.status).json(response.data);
-      }
-
-
+      res.status(response.status).json(response.data);
+    }
 
     res.status(response.status).json(response.data);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
   }
 }
