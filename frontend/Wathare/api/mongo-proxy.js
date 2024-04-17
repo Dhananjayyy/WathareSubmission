0db78c922990da8bdd095262a57e020b5f8422bb
Dhananjay Yelwande <yelwandedhananjay@gmail.com>
2024-04-18 02:31:06 +0530

    fix

--- frontend/Wathare/api/mongo-proxy.js
+++ frontend/Wathare/api/mongo-proxy.js
@@ -1,13 +1,13 @@
-import axios from'axios';
+import axios from 'axios';
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   try {
     const response = await axios({
       method: req.method,
       url: 'https://ap-south-1.aws.data.mongodb-api.com/app/data-iogss/endpoint/data/v1/action/findOne',
       headers: {
         'Content-Type': 'application/json',
-        'api-key': process.env.MONGODB_API_KEY
+        'api-key': process.env.MONGODB_API
       },
       data: req.body
     });
@@ -16,4 +16,4 @@ module.exports = async (req, res) => {
     console.error(error);
     res.status(500).json({ error: 'Internal server error' });
   }
-};
+}
