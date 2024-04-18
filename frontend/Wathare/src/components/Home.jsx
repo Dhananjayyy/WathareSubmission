ba582b007bd9a39e1df6d40256776e7c370bda32
Dhananjay Yelwande <yelwandedhananjay@gmail.com>
2024-04-18 06:42:47 +0530

    fix

--- frontend/Wathare/src/components/Home.jsx
+++ frontend/Wathare/src/components/Home.jsx
@@ -53,24 +53,27 @@ export default function Home() {
 
   return (
     <div className="container mt-3 mb-3">
-      <h1>Wathare Infotech Solutions Submission</h1>
-      <div className="text-right">
+    <div className="text-center content-center">
+        <h1>Wathare Infotech Solutions Submission</h1>
+    </div>
+    <div className="d-flex justify-content-end">  <!-- Updated this line -->
         <button className="btn btn-light m-2" onClick={() => fetchData(1)}>1 hr</button>
         <button className="btn btn-primary m-2" onClick={() => fetchData(8)}>8 hr</button>
         <button className="btn btn-dark m-2" onClick={() => fetchData(24)}>24 hr</button>
-      </div>
-      <div className="text-center content-center">
+    </div>
+    <div className="text-center content-center">
         <h2>Cycle Status</h2>
         {loading ? <p>Loading...</p> : <MyD3Chart data={data} />}
-      </div>
-      <div className="text-center content-center">
+    </div>
+    <div className="text-center content-center">
         <h2>Temperature</h2>
         {loading ? <p>Loading...</p> : <LocationTemperature />}
-      </div>
-      <div className="text-center content-center">
+    </div>
+    <div className="text-center content-center">
         <h2> Simulator </h2>
         <Simulator />
-      </div>
     </div>
+</div>
+
   );
 }
