c0b0fe4e8fc7a21aaac619a13414f7d3d9fd6b5f
Dhananjay Yelwande <yelwandedhananjay@gmail.com>
2024-04-18 08:32:36 +0530

    fix

--- frontend/Wathare/src/components/Simulator.jsx
+++ frontend/Wathare/src/components/Simulator.jsx
@@ -14,21 +14,20 @@ const Simulator = () => {
 
   const simulateData = () => {
     const simulatedData = [];
+    const startTimeMillis = new Date(startTime).getTime();
   
     if (!startTime) {
       alert('Please select a valid start time.');
       return;
     }
-  
-    const startTimeMillis = new Date(startTime).getTime();
-    let timestamp = startTimeMillis;
+    let lastTimestamp = startTimeMillis;
   
     for (let i = 0; i < numEntries; i++) {
-      const formattedTimestamp = new Date(timestamp).toISOString();
+      const timestamp = new Date(lastTimestamp).toISOString();
       const machineStatus = Math.floor(Math.random() * 2);
       const vibration = Math.floor(Math.random() * (vibrationRange.max - vibrationRange.min) + vibrationRange.min);
-      simulatedData.push({ ts: formattedTimestamp, machine_status: machineStatus, vibration: vibration });
-      timestamp += 1000;
+      simulatedData.push({ ts: timestamp, machine_status: machineStatus, vibration: vibration });
+      lastTimestamp += 1000;
     }
   
     setData(simulatedData);
@@ -36,8 +35,8 @@ const Simulator = () => {
   };
 
   return (
-    <div className="container text-center content-center" style={{ justifyContent: "center", width: "80%", margin: "auto", textAlign: "center" }}>
- <div className="row">
+    <div className="container text-center content-center" style={{justifyContent: "center", width: "50%", margin: "auto", textAlign: "center" }}>
+      <div className="row">
         <div className="col">
           <label className="form-label">Start Time:</label>
           <input type="datetime-local" className="form-control" value={startTime} onChange={(e) => setStartTime(e.target.value)} />
