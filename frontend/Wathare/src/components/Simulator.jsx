f61134e1f979620bdd7727c90a54c4f66d3aee30
Dhananjay Yelwande <yelwandedhananjay@gmail.com>
2024-04-18 09:00:09 +0530

    fix

--- frontend/Wathare/src/components/Simulator.jsx
+++ frontend/Wathare/src/components/Simulator.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import MyD3Chart from "./MyD3Chart";
 
 const Simulator = () => {
-  const [startTime, setStartTime] = useState('');
+  const [startTime, setStartTime] = useState("");
   const [vibrationRange, setVibrationRange] = useState({ min: 500, max: 1000 });
   const [numEntries, setNumEntries] = useState(10);
   const [data, setData] = useState(null);
@@ -10,7 +10,7 @@ const Simulator = () => {
 
   const viewSimulation = () => {
     setShowSimulation(true);
-  }
+  };
 
   const simulateData = (startTime) => {
     const simulatedData = [];
@@ -18,69 +18,107 @@ const Simulator = () => {
     const startTimeMillis = startDate.getTime(); // Get the start time in milliseconds
 
     if (isNaN(startTimeMillis)) {
-        alert('Please enter a valid start time.');
-        return;
+      alert("Please enter a valid start time.");
+      return;
     }
     let lastTimestamp = startTimeMillis;
 
     for (let i = 0; i < numEntries; i++) {
-        const timestamp = new Date(lastTimestamp).toISOString();
-        const machineStatus = Math.floor(Math.random() * 2);
-        const vibration = Math.floor(Math.random() * (vibrationRange.max - vibrationRange.min) + vibrationRange.min);
-        simulatedData.push({ ts: timestamp, machine_status: machineStatus, vibration: vibration });
-        lastTimestamp += 1000;
+      const timestamp = new Date(lastTimestamp).toISOString();
+      const machineStatus = Math.floor(Math.random() * 2);
+      const vibration = Math.floor(
+        Math.random() * (vibrationRange.max - vibrationRange.min) +
+          vibrationRange.min
+      );
+      simulatedData.push({
+        ts: timestamp,
+        machine_status: machineStatus,
+        vibration: vibration,
+      });
+      lastTimestamp += 1000;
     }
 
     setData(simulatedData);
     console.log("simulated data: " + JSON.stringify(simulatedData));
     setShowSimulation(true);
-};
-
+  };
 
   return (
-    <div className="container text-center content-center" style={{justifyContent: "center", width: "50%", margin: "auto", textAlign: "center" }}>
-      <div className="row">
-        <div className="col">
-          <label className="form-label">Start Time:</label>
-          <input type="datetime-local" className="form-control" value={startTime} onChange={(e) => setStartTime(e.target.value)} />
+    <div>
+      <div
+        className="container text-center content-center"
+        style={{
+          justifyContent: "center",
+          width: "50%",
+          margin: "auto",
+          textAlign: "center",
+        }}
+      >
+        <div className="row">
+          <div className="col">
+            <label className="form-label">Start Time:</label>
+            <input
+              type="datetime-local"
+              className="form-control"
+              value={startTime}
+              onChange={(e) => setStartTime(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="row" style={{ marginTop: "10px" }}>
+          <div className="col-md-6">
+            <label className="form-label">Min Vibration:</label>
+            <input
+              type="number"
+              className="form-control"
+              value={vibrationRange.min}
+              onChange={(e) =>
+                setVibrationRange({
+                  ...vibrationRange,
+                  min: parseInt(e.target.value),
+                })
+              }
+            />
+          </div>
+          <div className="col-md-6">
+            <label className="form-label">Max Vibration:</label>
+            <input
+              type="number"
+              className="form-control"
+              value={vibrationRange.max}
+              onChange={(e) =>
+                setVibrationRange({
+                  ...vibrationRange,
+                  max: parseInt(e.target.value),
+                })
+              }
+            />
+          </div>
+        </div>
+        <div className="row" style={{ marginTop: "10px" }}>
+          <div className="col">
+            <label className="form-label">Number of Entries:</label>
+            <input
+              type="number"
+              className="form-control"
+              value={numEntries}
+              onChange={(e) => setNumEntries(parseInt(e.target.value))}
+            />
+          </div>
         </div>
       </div>
-      <div className="row" style={{ marginTop: "10px" }}>
-        <div className="col-md-6">
-          <label className="form-label">Min Vibration:</label>
-          <input
-            type="number"
-            className="form-control"
-            value={vibrationRange.min}
-            onChange={(e) => setVibrationRange({ ...vibrationRange, min: parseInt(e.target.value) })}
-          />
-        </div>
-        <div className="col-md-6">
-          <label className="form-label">Max Vibration:</label>
-          <input
-            type="number"
-            className="form-control"
-            value={vibrationRange.max}
-            onChange={(e) => setVibrationRange({ ...vibrationRange, max: parseInt(e.target.value) })}
-          />
-        </div>
+<div className="">
+      <div className="">
+        <button className="btn btn-primary" onClick={simulateData}>
+          View Simulation
+        </button>
       </div>
-      <div className="row" style={{ marginTop: "10px" }}>
-        <div className="col">
-          <label className="form-label">Number of Entries:</label>
-          <input type="number" className="form-control" value={numEntries} onChange={(e) => setNumEntries(parseInt(e.target.value))} />
-        </div>
-      </div>
-      <div className="row" style={{ marginTop: "10px" }}>
-        <div className="col">
-          <button className="btn btn-primary" onClick={simulateData}>View Simulation</button>
-        </div>
-        {showSimulation && (
-          <div className="col" style={{ marginTop: "10px" }}>
-            <MyD3Chart data={data} style={{ width: "100%" }} />
+      {showSimulation && (
+        <div className="" style={{ marginTop: "10px" }}>
+          <MyD3Chart data={data} style={{ width: "100%" }} />
         </div>
       )}
-      </div>
+    </div>
     </div>
   );
 };
